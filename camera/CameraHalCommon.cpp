2e97c4af00e4e6fbeeaf568e67f0d664e1d6e9d8
Georgi Georgiev <x0154504@ti.com>
2012-07-19 17:41:05 +0300

    CameraHAL: Enabling PPM prints in Logcat by default
    Port of ICS change  Ifbd23a09e33e1a634505a344ec3151fcb55a405
    
    Change-Id: I774e28ae579bcecca0f2c15f98842d0ffef7079b
    Signed-off-by: Georgi Georgiev <x0154504@ti.com>

--- camera/CameraHalCommon.cpp
+++ camera/CameraHalCommon.cpp
@@ -47,7 +47,7 @@ void CameraHal::PPM(const char* str){
     ppm.tv_sec = ppm.tv_sec * 1000000;
     ppm.tv_sec = ppm.tv_sec + ppm.tv_usec - ppm_start.tv_usec;
 
-    LOGD("PPM: %s :%ld.%ld ms", str, ( ppm.tv_sec /1000 ), ( ppm.tv_sec % 1000 ));
+    CAMHAL_LOGI("PPM: %s :%ld.%ld ms", str, ( ppm.tv_sec /1000 ), ( ppm.tv_sec % 1000 ));
 }
 
 #elif PPM_INSTRUMENTATION_ABS
@@ -77,7 +77,7 @@ void CameraHal::PPM(const char* str){
     absolute *= 1000;
     absolute += ppm.tv_usec /1000;
 
-    CAMHAL_LOGD("PPM: %s :%llu.%llu ms : %llu ms", str, ( elapsed /1000 ), ( elapsed % 1000 ), absolute);
+    CAMHAL_LOGI("PPM: %s :%llu.%llu ms : %llu ms", str, ( elapsed /1000 ), ( elapsed % 1000 ), absolute);
 }
 
 #endif
@@ -110,7 +110,7 @@ void CameraHal::PPM(const char* str, struct timeval* ppm_first, ...){
     ppm.tv_sec = ppm.tv_sec * 1000000;
     ppm.tv_sec = ppm.tv_sec + ppm.tv_usec - ppm_first->tv_usec;
 
-    CAMHAL_LOGD("PPM: %s :%ld.%ld ms :  %llu ms", temp_str, ( ppm.tv_sec /1000 ), ( ppm.tv_sec % 1000 ), absolute);
+    CAMHAL_LOGI("PPM: %s :%ld.%ld ms :  %llu ms", temp_str, ( ppm.tv_sec /1000 ), ( ppm.tv_sec % 1000 ), absolute);
 
     va_end(args);
 }
