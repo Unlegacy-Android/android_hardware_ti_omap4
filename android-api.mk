e09030d0113f166d22ad6ba440baae29af0d0118
Daniel Levin <dendy@ti.com>
2012-11-21 14:25:22 +0200

    Add ANDROID_API_JB_MR1_OR_LATER
    Makefile variable and C/C++ macro to recognize API level 17.
    
    Change-Id: Ia95de90b6b83a79625c222e2e0dde63541cd3d7c
    Signed-off-by: Daniel Levin <dendy@ti.com>

--- android-api.mk
+++ android-api.mk
@@ -1,9 +1,14 @@
 
 # Makefile variables and C/C++ macros to recognize API level
+ANDROID_API_JB_MR1_OR_LATER :=
 ANDROID_API_JB_OR_LATER :=
 ANDROID_API_ICS_OR_LATER :=
 ANDROID_API_CFLAGS :=
 
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 17 || echo 1),)
+    ANDROID_API_JB_MR1_OR_LATER := true
+    ANDROID_API_CFLAGS += -DANDROID_API_JB_MR1_OR_LATER
+endif
 ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 16 || echo 1),)
     ANDROID_API_JB_OR_LATER := true
     ANDROID_API_CFLAGS += -DANDROID_API_JB_OR_LATER
@@ -14,6 +19,7 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 14 || echo 1),)
 endif
 
 define clear-android-api-vars
+$(eval ANDROID_API_JB_MR1_OR_LATER:=) \
 $(eval ANDROID_API_JB_OR_LATER:=) \
 $(eval ANDROID_API_ICS_OR_LATER:=) \
 $(eval ANDROID_API_CFLAGS:=) \
