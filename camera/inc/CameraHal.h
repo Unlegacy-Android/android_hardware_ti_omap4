dendy/camerahal

CameraHAL: Reduce metadata callbacks rate
Fixes regression introduced with change I3e5eec25:
Revert "CameraHAL: Adds support for controlling
the FD update interval"

Currently, CameraHal is emmiting preview metadata callbacks
to the Camera application with every preview frame.
But in most cases, there is no any difference between the metadata
of two consecutive frames. So, tracking of metadata changes
would be used in order to send metadata callbacks only if
metadata is different compared to one already sent to
the application. Ie93c6ae5 (Adds support for controlling the FD
update interval) had a disadvantage to add undesired delay when
sending metadata in range (0 ~ 400 ms).

Change-Id: I481a78020a040153f792a3ef9e095b59ea7003e0
Signed-off-by: Vladimir Petrov <vppetrov@mm-sol.com>

camera/inc/CameraHal.h
@@ -249,24 +249,23 @@ class CameraMetadataResult : public android::RefBase
 {
 public:
 
-    CameraMetadataResult() : mMetadata(NULL) {};
-    CameraMetadataResult(camera_frame_metadata_t *meta) : mMetadata(meta) {};
+    CameraMetadataResult() {
+        mMetadata.faces = NULL;
+        mMetadata.number_of_faces = 0;
+#ifdef OMAP_ENHANCEMENT
+        mMetadata.analog_gain = 0;
+        mMetadata.exposure_time = 0;
+#endif
+
+   }
 
     virtual ~CameraMetadataResult() {
-        if ( ( NULL != mMetadata ) && ( NULL != mMetadata->faces ) ) {
-            free(mMetadata->faces);
-            free(mMetadata);
-            mMetadata=NULL;
+        if ( NULL != mMetadata.faces ) {
+            free(mMetadata.faces);
         }
-
-        if(( NULL != mMetadata ))
-            {
-            free(mMetadata);
-            mMetadata = NULL;
-            }
     }
 
-    camera_frame_metadata_t *getMetadataResult() { return mMetadata; };
+    camera_frame_metadata_t *getMetadataResult() { return &mMetadata; };
 
     static const ssize_t TOP = -1000;
     static const ssize_t LEFT = -1000;
@@ -276,7 +275,7 @@ public:
 
 private:
 
-    camera_frame_metadata_t *mMetadata;
+    camera_frame_metadata_t mMetadata;
 };
 
 typedef enum {

Unlegacy-Android/android_hardware_ti_omap4

camera/inc/CameraHal.h
@@ -249,24 +249,23 @@ class CameraMetadataResult : public android::RefBase
 {
 public:
 
-    CameraMetadataResult() : mMetadata(NULL) {};
-    CameraMetadataResult(camera_frame_metadata_t *meta) : mMetadata(meta) {};
+    CameraMetadataResult() {
+        mMetadata.faces = NULL;
+        mMetadata.number_of_faces = 0;
+#ifdef OMAP_ENHANCEMENT_CPCAM
+        mMetadata.analog_gain = 0;
+        mMetadata.exposure_time = 0;
+#endif
+
+   }
 
     virtual ~CameraMetadataResult() {
-        if ( ( NULL != mMetadata ) && ( NULL != mMetadata->faces ) ) {
-            free(mMetadata->faces);
-            free(mMetadata);
-            mMetadata=NULL;
+        if ( NULL != mMetadata.faces ) {
+            free(mMetadata.faces);
         }
-
-        if(( NULL != mMetadata ))
-            {
-            free(mMetadata);
-            mMetadata = NULL;
-            }
     }
 
-    camera_frame_metadata_t *getMetadataResult() { return mMetadata; };
+    camera_frame_metadata_t *getMetadataResult() { return &mMetadata; };
 
     static const ssize_t TOP = -1000;
     static const ssize_t LEFT = -1000;
@@ -276,7 +275,7 @@ public:
 
 private:
 
-    camera_frame_metadata_t *mMetadata;
+    camera_frame_metadata_t mMetadata;
 };
 
 typedef enum {