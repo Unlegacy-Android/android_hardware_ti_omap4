6247a22ce7a7a36b0a984378ccaa494c767de9e3
Hashcode <hashcode0f@gmail.com>
2015-02-03 13:24:36 -0800

    pvrsrvinit: remove hard-coded lib location and adjust libs by gpu type

--- pvrsrvinit/Android.mk
+++ pvrsrvinit/Android.mk
@@ -3,9 +3,17 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := pvrsrvinit.c
-LOCAL_LDFLAGS := -L vendor/motorola/omap4-common/imgtec/vendor/lib
+LOCAL_LDFLAGS := -L $(TARGET_OUT_VENDOR)/lib
+ifeq ($(TARGET_BOARD_OMAP_CPU),4470)
+LOCAL_LDLIBS := -lsrv_init_SGX544_112 -lsrv_um_SGX544_112
+pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_init_SGX544_112.so
+pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_um_SGX544_112.so
+else
 LOCAL_LDLIBS := -lsrv_init_SGX540_120 -lsrv_um_SGX540_120
-LOCAL_MODULE_PATH := $(TARGET_OUT)/bin/
+pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_init_SGX540_120.so
+pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_um_SGX540_120.so
+endif
+LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR_EXECUTABLES)
 LOCAL_MODULE := pvrsrvinit
 LOCAL_MODULE_TAGS := optional
 
