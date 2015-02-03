b1047350b5b678c44e89d47e8a7faa14f87a16b2
Hashcode <hashcode0f@gmail.com>
2015-02-03 13:20:33 -0800

    symlinks: add symlinks for libion and gralloc based on TARGET_BOARD_OMAP_CPU

---
+++ symlinks/Android.mk
@@ -0,0 +1,36 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+#Creating Gralloc SymLink
+GRALLOC_SYMLINK := $(TARGET_OUT_VENDOR)/lib/hw/gralloc.$(TARGET_BOARD_PLATFORM).so
+$(GRALLOC_SYMLINK): GRALLOC_FILE := gralloc.omap$(TARGET_BOARD_OMAP_CPU).so
+$(GRALLOC_SYMLINK): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> $(GRALLOC_FILE)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
+	@rm -rf $@
+	$(hide) ln -fs $(GRALLOC_FILE) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GRALLOC_SYMLINK)
+
+# for mm/mmm
+all_modules: $(GRALLOC_SYMLINK)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libion.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := FAKE
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+$(LOCAL_BUILT_MODULE): TARGET := /system/lib/libion_ti.so
+$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT)/lib/$(LOCAL_MODULE)
+$(LOCAL_BUILT_MODULE):
+	$(hide) echo "Symlink: $(SYMLINK) -> $(TARGET)"
+	$(hide) mkdir -p $(dir $@)
+	$(hide) mkdir -p $(dir $(SYMLINK))
+	$(hide) rm -rf $@
+	$(hide) rm -rf $(SYMLINK)
+	$(hide) ln -sf $(TARGET) $(SYMLINK)
+	$(hide) touch $@
+
