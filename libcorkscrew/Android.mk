c3414798ae59ab7f6d8f4f00e50fa84238b12c55
Kyle Repinski <repinski23@gmail.com>
2016-02-28 15:58:17 -0600

    libcorkscrew: Build with -Wall -Werror.

--- libcorkscrew/Android.mk
+++ libcorkscrew/Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := corkscrew.c
-LOCAL_SHARED_LIBRARIES:= libc
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_SHARED_LIBRARIES := libc
+LOCAL_CFLAGS := -Wall -Werror
 LOCAL_MODULE := libcorkscrew
 LOCAL_MODULE_TAGS := optional
 
--- libcorkscrew/corkscrew.c
+++ libcorkscrew/corkscrew.c
@@ -21,40 +21,44 @@
 
 #include "corkscrew.h"
 
-ssize_t unwind_backtrace(void* backtrace, size_t ignore_depth, size_t max_depth)
+ssize_t unwind_backtrace(void* backtrace __unused,
+        size_t ignore_depth __unused, size_t max_depth __unused)
 {
 	return -1;
 }
 
-ssize_t unwind_backtrace_thread(pid_t tid, void* backtrace,
-        size_t ignore_depth, size_t max_depth)
+ssize_t unwind_backtrace_thread(pid_t tid __unused, void* backtrace __unused,
+        size_t ignore_depth __unused, size_t max_depth __unused)
 {
 	return -1;
 }
 
-ssize_t unwind_backtrace_ptrace(pid_t tid, const void* context,
-        void* backtrace, size_t ignore_depth, size_t max_depth)
+ssize_t unwind_backtrace_ptrace(pid_t tid __unused,
+        const void* context __unused, void* backtrace __unused,
+        size_t ignore_depth __unused, size_t max_depth __unused)
 {
 	return -1;
 }
 
-void get_backtrace_symbols(const void* backtrace, size_t frames,
-        void* backtrace_symbols)
+void get_backtrace_symbols(const void* backtrace __unused,
+        size_t frames __unused, void* backtrace_symbols __unused)
 {
 }
 
-void get_backtrace_symbols_ptrace(const void* context,
-        const void* backtrace, size_t frames,
-        void* backtrace_symbols)
+void get_backtrace_symbols_ptrace(const void* context __unused,
+        const void* backtrace __unused, size_t frames __unused,
+        void* backtrace_symbols __unused)
 {
 }
 
-void free_backtrace_symbols(void* backtrace_symbols, size_t frames)
+void free_backtrace_symbols(void* backtrace_symbols __unused,
+        size_t frames __unused)
 {
 }
 
-void format_backtrace_line(unsigned frameNumber, const void* frame,
-        const void* symbol, char* buffer, size_t bufferSize)
+void format_backtrace_line(unsigned frameNumber __unused,
+        const void* frame __unused, const void* symbol __unused,
+        char* buffer, size_t bufferSize)
 {
 	if (bufferSize > 0)
 		buffer[0] = 0x00;
