a5041c9e29f7bac325d4b572f8104bf42c046d94
Kyle Repinski <repinski23@gmail.com>
2016-02-28 16:11:53 -0600

    libcorkscrew: Remove unnecessary LOCAL_SHARED_LIBRARIES.
    libc is automatically included.

--- libcorkscrew/Android.mk
+++ libcorkscrew/Android.mk
@@ -3,7 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := corkscrew.c
-LOCAL_SHARED_LIBRARIES := libc
 LOCAL_CFLAGS := -Wall -Werror
 LOCAL_MODULE := libcorkscrew
 LOCAL_MODULE_TAGS := optional
