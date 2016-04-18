bec210e6725922abd46ca65ae335be5a226e5e24
D. Andrei Măceș <dmaces@nd.edu>
2016-04-18 14:38:37 -0400

    pvrsrvinit: PVRScopeServices symlink lives in vendor/ti now
    Change-Id: I31b4139e3607a8bd7b4d024f6e80b2bb3d03c66f

--- pvrsrvinit/Android.mk
+++ pvrsrvinit/Android.mk
@@ -20,23 +20,3 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
 
-#Create PVR SymLink
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libPVRScopeServices.so
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := FAKE
-
-include $(BUILD_SYSTEM)/base_rules.mk
-
-$(LOCAL_BUILT_MODULE): PVR_FILE := libPVRScopeServices_SGX540_120.so
-$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT_VENDOR)/lib/$(LOCAL_MODULE)
-$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/Android.mk
-$(LOCAL_BUILT_MODULE):
-	$(hide) echo "Symlink: $(SYMLINK) -> $(PVR_FILE)"
-	$(hide) mkdir -p $(dir $@)
-	$(hide) mkdir -p $(dir $(SYMLINK))
-	$(hide) rm -rf $@
-	$(hide) rm -rf $(SYMLINK)
-	$(hide) ln -sf $(PVR_FILE) $(SYMLINK)
-	$(hide) touch $@
