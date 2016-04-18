c24c680cec7e0f935322fc021622a6097c55b3f7
D. Andrei Măceș <dmaces@nd.edu>
2016-04-18 14:47:42 -0400

    pvrsrvinit/libEGL_POWERVR: Link against generic library symlinks
    Since newer DDKs may expect more generic UM prebuilt names, link
    against the symlinks prepared in vendor/ti, rather than matching
    against TARGET_BOARD_OMAP_CPU every time.
    
    Change-Id: Ibc6a3b4f92984f787ddbe9eba596875b9fbd7c26

--- pvrsrvinit/Android.mk
+++ pvrsrvinit/Android.mk
@@ -5,15 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := pvrsrvinit.c
 LOCAL_CFLAGS := -Wall -Werror
 LOCAL_LDFLAGS := -L $(TARGET_OUT_VENDOR)/lib
-ifeq ($(TARGET_BOARD_OMAP_CPU),4470)
-LOCAL_LDFLAGS += -lsrv_init_SGX544_112 -lsrv_um_SGX544_112
-pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_init_SGX544_112.so
-pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_um_SGX544_112.so
-else
-LOCAL_LDFLAGS += -lsrv_init_SGX540_120 -lsrv_um_SGX540_120
-pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_init_SGX540_120.so
-pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_um_SGX540_120.so
-endif
+LOCAL_LDFLAGS += -lsrv_init -lsrv_um
+pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_init.so
+pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_um.so
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE := pvrsrvinit
 LOCAL_MODULE_TAGS := optional
