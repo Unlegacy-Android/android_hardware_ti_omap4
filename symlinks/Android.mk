1a3ebf7ba398ac964746ea9cc60259e93af3cf02
D. Andrei Măceș <dmaces@nd.edu>
2016-04-18 14:24:18 -0400

    symlinks: Move Gralloc's to vendor/ti (together with processing)
    Change-Id: I9fb13a110e80b8775ec7f1319641638b6fc908b3

--- symlinks/Android.mk
+++ symlinks/Android.mk
@@ -1,22 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-#Creating Gralloc SymLink
-GRALLOC_SYMLINK := $(TARGET_OUT_VENDOR)/lib/hw/gralloc.$(TARGET_BOARD_PLATFORM).so
-$(GRALLOC_SYMLINK): GRALLOC_FILE := gralloc.omap$(TARGET_BOARD_OMAP_CPU).so
-$(GRALLOC_SYMLINK): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
-	@echo "Symlink: $@ -> $(GRALLOC_FILE)"
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
-	@rm -rf $@
-	$(hide) ln -fs $(GRALLOC_FILE) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(GRALLOC_SYMLINK)
-
-# for mm/mmm
-all_modules: $(GRALLOC_SYMLINK)
-
-include $(CLEAR_VARS)
-
 ifeq ($(BOARD_USE_TI_LIBION),true)
 
 LOCAL_MODULE := libion.so
