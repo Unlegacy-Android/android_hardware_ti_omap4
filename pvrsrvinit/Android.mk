f47df7b0ebdd33e33962701adbd4d1300c36e081
D. Andrei Măceș <dmaces@nd.edu>
2016-04-21 14:38:38 -0400

    pvrsrvinit/libEGL_POWERVR: Actually ready needed vendor libraries
    Change-Id: I7c3dc8abfd9367b1b983b696d7357a27f9350b60

--- pvrsrvinit/Android.mk
+++ pvrsrvinit/Android.mk
@@ -5,9 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := pvrsrvinit.c
 LOCAL_CFLAGS := -Wall -Werror
 LOCAL_LDFLAGS := -L $(TARGET_OUT_VENDOR)/lib
-LOCAL_LDFLAGS += -lsrv_init -lsrv_um
-pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_init.so
-pvrsrvinit: $(TARGET_OUT_VENDOR)/lib/libsrv_um.so
+LOCAL_ADDITIONAL_DEPENDENCIES := libsrv_init libsrv_um
+LOCAL_LDFLAGS += $(LOCAL_ADDITIONAL_DEPENDENCIES:lib%=-l%)
 LOCAL_MODULE_PATH := $(TARGET_OUT_EXECUTABLES)
 LOCAL_MODULE := pvrsrvinit
 LOCAL_MODULE_TAGS := optional
