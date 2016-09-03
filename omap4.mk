bc08f40ee843687b9a3f4193c04a6b937c757fa6
Ziyan <jaraidaniel@gmail.com>
2016-09-03 23:15:32 +0200

    omap4: add more common build properties

--- omap4.mk
+++ omap4.mk
@@ -26,6 +26,26 @@ PRODUCT_VENDOR_KERNEL_HEADERS := hardware/ti/omap4/kernel-headers
 PRODUCT_COPY_FILES += \
     $(OMAP4_NEXT_FOLDER)/rootdir/init.omap4.rc:root/init.omap4.rc
 
+# SGX540 is slower with the scissor optimization enabled
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.hwui.disable_scissor_opt=true
+
+# Disable dirty regions invalidation
+ADDITIONAL_BUILD_PROPERTIES += \
+    hwui.render_dirty_regions=false
+
+# We don't support eglSwapBuffersWithDamageKHR; this avoids some unnecessary code
+ADDITIONAL_BUILD_PROPERTIES += \
+    debug.hwui.swap_with_damage=false
+
+# We don't support the new camera API
+ADDITIONAL_BUILD_PROPERTIES += \
+    camera2.portability.force_api=1
+
+# GPU producer to CPU consumer not supported
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.bq.gpu_to_cpu_unsupported=1
+
 $(call inherit-product, hardware/ti/omap4/common.mk)
 $(call inherit-product-if-exists, vendor/ti/omap4/omap4-vendor.mk)
 $(call inherit-product-if-exists, vendor/widevine/omap4/widevine-vendor.mk)
