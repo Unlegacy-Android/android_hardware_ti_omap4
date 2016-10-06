f48fac0d5ca7021c61d5c8cece5dcbb0eb1818bf
D. Andrei Măceș <dmaces@nd.edu>
2016-10-05 22:30:41 -0400

    common: Choose libEGL shim based on ANDROID_API_LP_OR_LATER
    Workaround for KitKat, which still needs the old TI shims.
    
    Change-Id: Id42340a6deab806fd07080efa2974b9a61b7a8d5

--- common.mk
+++ common.mk
@@ -49,8 +49,13 @@ endif
 PRODUCT_PACKAGES += \
     libion.so \
     pvrsrvinit \
-    libEGL_POWERVR_SGX54X \
     libPVRScopeServices.so
+
+ifeq ($(ANDROID_API_LP_OR_LATER),true)
+PRODUCT_PACKAGES += libEGL_POWERVR_SGX54X
+else
+PRODUCT_PACKAGES += libEGL_POWERVR
+endif
 
 # compatiblity for older binaries
 PRODUCT_PACKAGES += \
