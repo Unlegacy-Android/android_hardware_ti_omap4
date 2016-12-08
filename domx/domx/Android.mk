e6adec23aea0576cc53d8f8d6f78297ffe8e27b5
D. Andrei Măceș <dmaces@nd.edu>
2016-12-08 08:54:25 -0500

    libion: Purge USE_TI_LIBION support from build system
    DMA-buffered ION is already the default, all our current kernel trees
    support it, and the underlying plumbing is mostly gone in newer DDKs.
    
    Change-Id: I420c1464a1ecbdbe2eb28711d427402c05824ab1
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- domx/domx/Android.mk
+++ domx/domx/Android.mk
@@ -37,14 +37,9 @@ LOCAL_SHARED_LIBRARIES := \
     liblog \
     libcutils
 
-ifeq ($(BOARD_USE_TI_LIBION),true)
-LOCAL_SHARED_LIBRARIES += libion_ti
-LOCAL_CFLAGS += -DUSE_TI_LIBION
-else
 LOCAL_SHARED_LIBRARIES += libion
 LOCAL_SRC_FILES += ../../libion/ion_ti_custom.c
 LOCAL_C_INCLUDES += $(HARDWARE_TI_OMAP4_BASE)/libion
-endif
 
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),tuna)
 	LOCAL_CFLAGS += -DDOMX_TUNA
