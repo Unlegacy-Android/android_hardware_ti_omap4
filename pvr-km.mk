45c4b753df6e8a6a2c828c818e9f051158c9e4b3
D. Andrei Măceș <dmaces@nd.edu>
2016-12-25 00:01:18 -0500

    pvr-km/init: Adapt PVR_MODULES for newer DDK sources
    Should work with DDK 1.10 (Rhombus Tech), 1.12 (TI Open), or 1.14
    (IT/TI Open). This overwrites other omaplfb.ko, if generated.
    
    Change-Id: Ied57ed4e89df65085cd3060b2b7fb9fbadeff681
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- pvr-km.mk
+++ pvr-km.mk
@@ -13,14 +13,14 @@
 # limitations under the License.
 
 PVR_MODULES:
-	make clean -C $(OMAP4_NEXT_FOLDER)/pvr-source/eurasiacon/build/linux2/omap4430_android
-	make -j8 -C $(OMAP4_NEXT_FOLDER)/pvr-source/eurasiacon/build/linux2/omap4430_android \
+	make clean -C $(OMAP4_NEXT_FOLDER)/pvr-source/eurasiacon/build/linux2/omap_android
+	make -j8 -C $(OMAP4_NEXT_FOLDER)/pvr-source/eurasiacon/build/linux2/omap_android \
 			ARCH=arm $(if $(ARM_CROSS_COMPILE),$(ARM_CROSS_COMPILE),$(KERNEL_CROSS_COMPILE)) \
-			TARGET_SGX=$(if $(filter-out 4470,$(TARGET_BOARD_OMAP_CPU)),540,544sc) \
+			TARGET_DEVICE=blaze_tablet$(if $(filter-out 4470,$(TARGET_BOARD_OMAP_CPU)),,".4470") \
 			KERNELDIR=$(KERNEL_OUT) BUILD=release
-	mv $(KERNEL_OUT)/../../target/kbuild/pvrsrvkm.ko $(KERNEL_MODULES_OUT)
+	mv $(KERNEL_OUT)/../../target/kbuild/{pvrsrvkm,omaplfb}.ko $(KERNEL_MODULES_OUT)
 	$(if $(ARM_EABI_TOOLCHAIN),$(ARM_EABI_TOOLCHAIN)/arm-eabi-strip, \
 			$(KERNEL_TOOLCHAIN_PATH)strip) --strip-unneeded \
-			$(KERNEL_MODULES_OUT)/pvrsrvkm.ko
+			$(KERNEL_MODULES_OUT)/{pvrsrvkm,omaplfb}.ko
 
 TARGET_KERNEL_MODULES += PVR_MODULES
