c42f09092abfcc8f2cd71c33fc5c427528c7eec3
Andreas Blaesius <skate4life@gmx.de>
2017-01-26 09:56:29 -0500

    Add proper sized Wallpaper for Tablets
    OMAP4 DDK doesn't like textures larger than 2048px; let's resize
    the new tablet wallpapers so they fit within the limits
    (size based on previous wallpapers).
    
    Change-Id: I2eda0d30b8e9674cea6aa80dad20e3a28b038086

--- common.mk
+++ common.mk
@@ -23,6 +23,10 @@ ifeq ($(ANDROID_API_LP_OR_LATER),true)
 DEVICE_PACKAGE_OVERLAYS += hardware/ti/omap4/overlay
 endif
 
+ifneq (,$(strip $(wildcard vendor/unlegacy/AndroidProducts.mk)))
+DEVICE_PACKAGE_OVERLAYS += hardware/ti/omap4/overlay-custom
+endif
+
 PRODUCT_PACKAGES += \
     libdomx \
     libOMX_Core \
