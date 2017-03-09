cf08f0d0c6c703e46aadae8cfc2b0056ae49d77b
D. Andrei Măceș <dmaces@nd.edu>
2017-03-09 14:53:07 -0500

    SGX-BIN: build: android: Don't override PLATFORM_VERSION if supplied
    Fall back to old ICS default when no PLATFORM_VERSION info exists.
    
    Change-Id: I1199cac6bd14c789640e3def9ec04811189cacf0

--- pvr-source/eurasiacon/build/linux2/common/android/platform_version.mk
+++ pvr-source/eurasiacon/build/linux2/common/android/platform_version.mk
@@ -40,15 +40,10 @@
 
 # Figure out the version of Android we're building against.
 #
+ifeq ($(strip $(PLATFORM_VERSION)),)
 BUILD_PROP := $(ANDROID_PRODUCT_OUT)/system/build.prop
-ifeq ($(wildcard $(BUILD_PROP)),)
-$(warning *** No device prop file ($(BUILD_PROP)). Extracting from \
-	build/core/version_defaults.mk)
-# Android version information doesn't permeate here. Set it up manually,
-# but avoid including the whole of core/version_defaults.mk
-$(eval $(shell cat $(ANDROID_BUILD_TOP)/build/core/version_defaults.mk |\
-	grep 'PLATFORM_VERSION\s.*='))
-else
+BUILD_DEFS := $(ANDROID_BUILD_TOP)/build/core/version_defaults.mk
+ifneq ($(wildcard $(BUILD_PROP)),)
 # Extract version.release from the build.prop file. If it's not in the build.prop,
 # the Make variables won't be defined, and fallback handling will take place.
 #
@@ -56,6 +51,21 @@ else
 # 	sed -e 's,ro.build.version.release,PLATFORM_VERSION,'))
 PLATFORM_VERSION := $(shell grep '^ro.build.version.release=' \
 	$(BUILD_PROP) | cut -f2 -d'=' | cut -f1 -d'-')
+else ifneq ($(wildcard $(BUILD_DEFS)),)
+$(warning *** No device prop file ($(BUILD_PROP)). Extracting from \
+	build/core/version_defaults.mk)
+# Android version information doesn't permeate here. Set it up manually,
+# but avoid including the whole of core/version_defaults.mk
+#
+# $(eval $(shell cat $(ANDROID_BUILD_TOP)/build/core/version_defaults.mk |\
+# 	grep 'PLATFORM_VERSION\s.*='))
+PLATFORM_VERSION := $(strip $(shell grep 'PLATFORM_VERSION\s.*=' \
+	$(BUILD_DEFS) | cut -f2 -d'=' | cut -f1 -d'-'))
+else
+$(warning *** No device prop file ($(BUILD_PROP)) or build env \
+	($(BUILD_DEFS)). Falling back to ICS default)
+PLATFORM_VERSION := 4.0.3
+endif
 endif
 
 $(info PLATFORM_VERSION=$(PLATFORM_VERSION))
