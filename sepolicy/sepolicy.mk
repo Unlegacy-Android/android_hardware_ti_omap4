140631120a9f5564d6535001c18337ece0d5834b
Andreas Blaesius <skate4life@gmx.de>
2017-04-14 16:26:36 +0200

    OMAP4: sepolicy: fix 4.4 compatibility
    - gpu_device type exist in LP and newer, which breaks compiling on KitKat
    - Support asec containers getting mounted on KitKat
    
    Change-Id: I77c24fa34e5f2425bc2ce3940a41e6db0c7777fb

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,23 @@
+OMAP4_SEPOLICY_BASE := hardware/ti/omap4/sepolicy
+
+# 4.4
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -eq 19 || echo 1),)
+BOARD_SEPOLICY_UNION += \
+	file.te \
+	file_contexts \
+	init.te \
+	pvrsrvinit.te \
+	system_server.te
+endif
+
+# >=5.0
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 20 || echo 1),)
+BOARD_SEPOLICY_DIRS += $(OMAP4_SEPOLICY_BASE)/compat/lp
+endif
+
+# >=7.0
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 24 || echo 1),)
+BOARD_SEPOLICY_M4DEFS += android_api_lp_or_later=true
+endif
+
+BOARD_SEPOLICY_DIRS += $(OMAP4_SEPOLICY_BASE)
