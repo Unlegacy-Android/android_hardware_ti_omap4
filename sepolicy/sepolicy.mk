f2768717cd96ae068d73fe0dcd6ff5b5bb53bd28
Andreas Blaesius <skate4life@gmx.de>
2017-04-14 16:53:35 +0200

    omap4: sepolicy: adress cameraserver denials on N
    Change-Id: I85aa44861bf8dd4bb6f098e571ff5db4afde4533

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -14,4 +14,9 @@ ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 20 || echo 1),)
 BOARD_SEPOLICY_DIRS += $(OMAP4_SEPOLICY_BASE)/compat/lp
 endif
 
+# >=7.0
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 24 || echo 1),)
+BOARD_SEPOLICY_M4DEFS += android_api_n_or_later=true
+endif
+
 BOARD_SEPOLICY_DIRS += $(OMAP4_SEPOLICY_BASE)
