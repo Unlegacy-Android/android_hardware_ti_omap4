4d11b063dc6520e427781515beb2a543c488ed7c
Andreas Blaesius <skate4life@gmx.de>
2017-05-01 19:24:57 +0100

    sepolicy: fix 5.1 compatibillity
    Change-Id: Ice4431816fe278dd5951eb6f4a85b3af818c08c9

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,12 +1,19 @@
 OMAP4_SEPOLICY_BASE := hardware/ti/omap4/sepolicy
 
-# 4.4
-ifeq ($(shell test $(PLATFORM_SDK_VERSION) -eq 19 || echo 1),)
+# <=5.1
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -lt 23 || echo 1),)
 BOARD_SEPOLICY_UNION += \
 	file.te \
 	file_contexts \
 	init.te \
 	pvrsrvinit.te
+
+# 5.x
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 20 || echo 1),)
+BOARD_SEPOLICY_UNION += \
+	defines.te \
+	system_server.te
+endif
 endif
 
 # >=5.0
