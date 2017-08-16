abbc07309ab7d4e516f64cfb87de202f669fd297
Andreas Blaesius <skate4life@gmx.de>
2017-08-16 11:52:21 +0100

    omap4: static tf_daemon & static smc_pa_ctrl for recovery
    - We need a static tf_daemon and smc_pa_ctrl for TWRP to be able
      to decrypt our data partition. Use TARGET_RECOVERY_DEVICE_MODULES
      in BoardConfig.mk to build a static tf_daemon and smc_pa_ctrl.
      TARGET_RECOVERY_DEVICE_MODULES := smc_pa_ctrl_static tf_daemon_static
    
    Change-Id: I9b444377b688a13542505bf6c2f971007d027366

--- security/smc_pa_ctrl/Android.mk
+++ security/smc_pa_ctrl/Android.mk
@@ -19,3 +19,31 @@ LOCAL_MODULE:= smc_pa_ctrl
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_EXECUTABLE)
+
+# static smc_pa_ctrl
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_SRC_FILES:= \
+	smc_pa_ctrl.c smc_pa_ctrl_linux.c
+
+LOCAL_CFLAGS += -DLINUX
+LOCAL_CFLAGS += -D__ANDROID32__
+
+ifdef S_VERSION_BUILD
+LOCAL_CFLAGS += -DS_VERSION_BUILD=$(S_VERSION_BUILD)
+endif
+
+LOCAL_CFLAGS += -I $(LOCAL_PATH)/../tf_sdk/include/
+
+LOCAL_MODULE:= smc_pa_ctrl_static
+LOCAL_MODULE_STEM := smc_pa_ctrl
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACK_MODULE_RELOCATIONS := false
+LOCAL_STATIC_LIBRARIES := libc libcutils liblog
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
+
+include $(BUILD_EXECUTABLE)
--- security/tf_daemon/Android.mk
+++ security/tf_daemon/Android.mk
@@ -3,16 +3,11 @@ include $(CLEAR_VARS)
 
 LOCAL_ARM_MODE := arm
 
-LOCAL_SRC_FILES := \
-	delegation_client.c \
-	delegation_client_extension.c \
-	smc_properties.c \
-	smc_properties_parser.c \
-	lib_manifest2.c
+LOCAL_SRC_FILES:= \
+	smc_pa_ctrl.c smc_pa_ctrl_linux.c
 
 LOCAL_CFLAGS += -DLINUX
 LOCAL_CFLAGS += -D__ANDROID32__
-LOCAL_CFLAGS += -DSUPPORT_DELEGATION_EXTENSION
 
 ifdef S_VERSION_BUILD
 LOCAL_CFLAGS += -DS_VERSION_BUILD=$(S_VERSION_BUILD)
@@ -20,8 +15,35 @@ endif
 
 LOCAL_CFLAGS += -I $(LOCAL_PATH)/../tf_sdk/include/
 
-LOCAL_MODULE:= tf_daemon
-LOCAL_STATIC_LIBRARIES := libtee_client_api_driver
+LOCAL_MODULE:= smc_pa_ctrl
 LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
+
+# static smc_pa_ctrl
+include $(CLEAR_VARS)
+
+LOCAL_ARM_MODE := arm
+
+LOCAL_SRC_FILES:= \
+	smc_pa_ctrl.c smc_pa_ctrl_linux.c
+
+LOCAL_CFLAGS += -DLINUX
+LOCAL_CFLAGS += -D__ANDROID32__
+
+ifdef S_VERSION_BUILD
+LOCAL_CFLAGS += -DS_VERSION_BUILD=$(S_VERSION_BUILD)
+endif
+
+LOCAL_CFLAGS += -I $(LOCAL_PATH)/../tf_sdk/include/
+
+LOCAL_MODULE:= smc_pa_ctrl_static
+LOCAL_MODULE_STEM := smc_pa_ctrl
+LOCAL_MODULE_TAGS := optional
+LOCAL_PACK_MODULE_RELOCATIONS := false
+LOCAL_STATIC_LIBRARIES := libc libcutils liblog
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 
 include $(BUILD_EXECUTABLE)
