17842da8b5adff881d60db66ffcc8fd8ca46d89f
Andreas Blaesius <skate4life@gmx.de>
2017-08-23 14:14:28 +0200

    libI420colorconvert: add libarect dependencie
    Fixes:the following build error on Android 8:
    ./frameworks/native/include/media/editor/II420ColorConverter.h:22:10: fatal error: 'android/rect.h' file not found
    
    Change-Id: I65f70bd059bc6e79be2a4e29a02b2a800dabf3fe

--- libI420colorconvert/Android.mk
+++ libI420colorconvert/Android.mk
@@ -12,6 +12,10 @@ LOCAL_CFLAGS := -Wall -Werror
 
 LOCAL_MODULE_TAGS := optional
 
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 || echo 1),)
+LOCAL_SHARED_LIBRARIES := libarect
+endif
+
 LOCAL_MODULE := libI420colorconvert
 
 include $(BUILD_HEAPTRACKED_SHARED_LIBRARY)
