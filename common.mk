10fbdaf576cd0548756bb2b8345f6ad87bd516c8
Ziyan <jaraidaniel@gmail.com>
2017-09-29 16:40:44 +0300

    common: Add required graphics HIDL implementations
    Change-Id: I047f668a350843124ead2a15c37911aac3634d44
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- common.mk
+++ common.mk
@@ -24,6 +24,10 @@ DEVICE_PACKAGE_OVERLAYS += hardware/ti/omap4/overlay
 endif
 
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
     libdomx \
     libOMX_Core \
     libOMX.TI.DUCATI1.VIDEO.H264E \
