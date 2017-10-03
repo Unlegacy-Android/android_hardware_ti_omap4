1c0cee7f7a0c7386da398e66667f5024f180ad51
D. Andrei Măceș <dmaces@nd.edu>
2017-10-03 22:07:03 +0300

    board/manifest: Copy a stub HIDL manifest via board flag
    We haven't binderized any HAL yet, so a device/vendor manifest
    wasn't essential, but this will quiet some logcat warnings, and
    will also prepare the tree for the upcoming Treblization.
    
    Change-Id: I9a4b6bb77fce25d14eb77aa89e0f96b5ebc219a5
    Signed-off-by: D. Andrei Măceș <andrei@unlegacy-android.org>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,6 +46,9 @@ TARGET_BOOTANIMATION_PRELOAD := false
 TARGET_BOOTANIMATION_TEXTURE_CACHE := false
 TARGET_BOOTANIMATION_USE_RGB565 := true
 
+# Partial HIDL manifest
+DEVICE_MANIFEST_FILE += $(OMAP4_NEXT_FOLDER)/manifest.xml
+
 # N-only system seccomp policy files
 BOARD_SECCOMP_POLICY += \
     $(OMAP4_NEXT_FOLDER)/seccomp
