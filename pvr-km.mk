1726d1330a483d8a7b6d898d22975aee50765d65
Andreas Blaesius <skate4life@gmx.de>
2017-12-02 14:11:20 +0100

    test
    Change-Id: I4c5c2caa9ae8b6751e360314e0ba723e27788749

--- pvr-km.mk
+++ pvr-km.mk
@@ -17,10 +17,11 @@ PVR_MODULES:
 	make -j8 -C $(OMAP4_NEXT_FOLDER)/pvr-source/eurasiacon/build/linux2/omap4430_android \
 			ARCH=arm $(if $(ARM_CROSS_COMPILE),$(ARM_CROSS_COMPILE),$(KERNEL_CROSS_COMPILE)) \
 			TARGET_SGX=$(if $(filter-out 4470,$(TARGET_BOARD_OMAP_CPU)),540,544sc) \
-			KERNELDIR=$(KERNEL_OUT) BUILD=release
+			$(KERNELDIR) BUILD=release
 	mv $(KERNEL_OUT)/../../target/kbuild/pvrsrvkm.ko $(KERNEL_MODULES_OUT)
 	$(if $(ARM_EABI_TOOLCHAIN),$(ARM_EABI_TOOLCHAIN)/arm-eabi-strip, \
 			$(KERNEL_TOOLCHAIN_PATH)strip) --strip-unneeded \
 			$(KERNEL_MODULES_OUT)/pvrsrvkm.ko
 
+KERNELDIR := $(KERNEL_OUT)
 TARGET_KERNEL_MODULES += PVR_MODULES
